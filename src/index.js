63225934da91a0844054f96a76ac8fbe3749028e
Tanvi Garg <tanvi.garg23@gmail.com>
2019-05-12 15:15:16 -0700

    add Link type resolver

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { GraphQLServer } = require('graphql-yoga')
+const { GraphQLServer } = require("graphql-yoga");
 
 const typeDefs = `
 type Query {
@@ -12,25 +12,31 @@ type Link {
   description: String!
   url: String!
 }
-`
+`;
 
-
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
+let links = [
+  
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL"
+  }
+];
 
 const resolvers = {
   Query: {
     info: () => null,
-    feed: () => links,
+    feed: () => links
+  },
+  Link: {
+    id: parent => parent.id,
+    description: parent => parent.description,
+    url: parent => parent.url
   }
-}
-
+};
 
 const server = new GraphQLServer({
   typeDefs,
-  resolvers,
-})
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+  resolvers
+});
+server.start(() => console.log(`Server is running on http://localhost:4000`));
